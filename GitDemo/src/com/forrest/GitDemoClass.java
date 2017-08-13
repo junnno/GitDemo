da6bcd6139c0979fc7a41c846567bd6bc25db14e
Junno <junnoberinguela@gmail.com>
2017-08-14 00:05:21 +0800

    testing

--- GitDemo/src/com/forrest/GitDemoClass.java
+++ GitDemo/src/com/forrest/GitDemoClass.java
@@ -9,6 +9,8 @@ public class GitDemoClass {
 		
 		System.out.println("This is the second change after downloading");
 		System.out.println("test");
+				System.out.println("test");
+
 		for (int i = 0; i<=10; i++) {
 			
 		}
